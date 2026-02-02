b2c566e8a78b6ff8ec0a3d6149818a70838d3083
Rohan Binu <rbinu2004@gmail.com>
2026-02-02 16:49:02 +0000

    removed token table + react frontend

--- backend/models/models.py
+++ backend/models/models.py
@@ -4,21 +4,16 @@ import uuid
 from typing import Optional
 
 
-class TimestampModel(SQLModel):
+class Base(SQLModel):
+    __abstract__ = True
+
     id: uuid.UUID = Field(
-        default_factory=uuid.uuid4, primary_key=True, index=True, nullable=False
-    )
-    created_at: datetime = Field(
-        sa_column=Column(DateTime(timezone=True), server_default=func.now())
-    )
-    updated_at: Optional[datetime] = Field(
-        sa_column=Column(
-            DateTime(timezone=True), onupdate=func.now(), server_default=func.now()
-        )
+        default_factory=uuid.uuid4,
+        primary_key=True,
     )
 
 
-class User(TimestampModel, table=True):
+class User(Base, table=True):
     email: str = Field(unique=True, index=True, nullable=False)
     hashed_password: str = Field(nullable=False)
     # is_active: bool = Field(default=True)
