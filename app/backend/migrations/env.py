e331eec112eaf84de65e43951befeebe68abb423
Rohan Binu <rbinu2004@gmail.com>
2026-03-28 22:44:04 +0000

    created positions table

--- app/backend/migrations/env.py
+++ app/backend/migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from sqlmodel import SQLModel
-from models.models import User, RefreshToken, Order
+from models.models import User, RefreshToken, Order, Position
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
